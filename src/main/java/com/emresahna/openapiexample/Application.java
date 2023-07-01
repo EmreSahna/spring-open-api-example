6cd3bccd67d86e33296d4abb21f2914c53038f8e
Emir Emre Sahna <emre.sahna@hotmail.com>
2023-07-01 18:58:04 +0300

    add: security scheme

--- src/main/java/com/emresahna/openapiexample/Application.java
+++ src/main/java/com/emresahna/openapiexample/Application.java
@@ -1,5 +1,7 @@
 package com.emresahna.openapiexample;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@SecurityScheme(name = "BearerJWT", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer", description = "JWT Authentication")
 public class Application {
 
 	public static void main(String[] args) {
