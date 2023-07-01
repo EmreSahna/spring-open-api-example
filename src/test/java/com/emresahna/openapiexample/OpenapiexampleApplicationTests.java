e5157d15a6f9acf8a52b21e1fcc77c48c694648c
Emir Emre Sahna <emre.sahna@hotmail.com>
2023-07-01 17:00:03 +0300

    init

---
+++ src/test/java/com/emresahna/openapiexample/OpenapiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emresahna.openapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
