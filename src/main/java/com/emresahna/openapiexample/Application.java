d63d65e1842098222f6a40384e0f2396f8c388dd
Emir Emre Sahna <emre.sahna@hotmail.com>
2023-07-01 18:57:33 +0300

    add: server & tag properties

--- src/main/java/com/emresahna/openapiexample/Application.java
+++ src/main/java/com/emresahna/openapiexample/Application.java
@@ -2,6 +2,8 @@ package com.emresahna.openapiexample;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +18,31 @@ public class Application {
 	@Bean
 	public OpenAPI customOpenAPI(){
 		return new OpenAPI()
-				.info(new Info()
-						.title("Example API")
-						.version("1.0.0")
-						.description("Example API Description"));
+				.info(
+						new Info()
+							.title("Social Media API")
+							.version("1.0.0")
+							.description("Social Media API Description")
+				)
+				.addServersItem(
+						new Server()
+							.url("http://localhost:8080")
+							.description("Development server")
+				)
+				.addServersItem(
+						new Server()
+							.url("https://api.example.com")
+							.description("Production server")
+				)
+				.addTagsItem(
+						new Tag()
+							.name("PostController")
+							.description("PostController API")
+				)
+				.addTagsItem(
+						new Tag()
+							.name("UserController")
+							.description("UserController API")
+				);
 	}
 }
